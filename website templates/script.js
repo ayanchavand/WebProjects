56849bcc2a74a7d29ecc1c3c9696a0bf8dd50aba
Ayan Chavand <ayan.chavand15@gmail.com>
2020-11-09 10:20:17 +0530

    first

---
+++ website templates/script.js
@@ -0,0 +1,8 @@
+function check(form){
+    if(form.user.value == "ayan" && form.pass.value== "12345"){
+        window.open('http://youtube.in/');
+    }
+    else{
+        alert("username or password incorrect");
+    }
+}    
